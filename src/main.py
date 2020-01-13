319f1d2c8c70734e0ce41303dc349f21097f22a8
sho-gun <masakihosono@gmail.com>
2020-01-13 15:43:33 +0900

    mnist prediction

--- src/main.py
+++ src/main.py
@@ -1,18 +1,20 @@
+import os
+import pickle
 import numpy as np
-from module.ActivationFunction import sigmoid, identityFunction
+from module.ActivationFunction import sigmoid, softmax
+from util.mnist import load_mnist
+
+def getData():
+    (xTrain, tTrain), (xTest, tTest) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
+    return xTest, tTest
 
 def initNetwork():
-    network = {}
-    network['W1'] = np.array([ [0.1, 0.3, 0.5], [0.2, 0.4, 0.6] ])
-    network['b1'] = np.array([0.1, 0.2, 0.3])
-    network['W2'] = np.array([ [0.1, 0.4], [0.2, 0.5], [0.3, 0.6] ])
-    network['b2'] = np.array([0.1, 0.2])
-    network['W3'] = np.array([ [0.1, 0.3], [0.2, 0.4] ])
-    network['b3'] = np.array([0.1, 0.2])
+    with open(os.path.join('weight', 'sample_weight.pkl'), 'rb') as f:
+        network = pickle.load(f)
 
     return network
 
-def forward(network, x):
+def predict(network, x):
     W1, W2, W3 = network['W1'], network['W2'], network['W3']
     b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
@@ -21,12 +23,21 @@ def forward(network, x):
     a2 = np.dot(z1, W2) + b2
     z2 = sigmoid(a2)
     a3 = np.dot(z2, W3) + b3
-    y = identityFunction(a3)
+    y = softmax(a3)
 
     return y
 
 if __name__ == '__main__':
+    x, t = getData()
     network = initNetwork()
-    x = np.array([1.0, 0.5])
-    y = forward(network, x)
-    print(y)
+
+    batch_size = 100
+    accuracy_cnt = 0
+
+    for i in range(0, len(x), batch_size):
+        x_batch = x[i:i+batch_size]
+        y_batch = predict(network, x_batch)
+        p = np.argmax(y_batch, axis=1)
+        accuracy_cnt += np.sum(p == t[i:i+batch_size])
+
+    print('Accuracy:' + str(float(accuracy_cnt) / len(x)))
