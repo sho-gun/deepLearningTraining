55ac63c5a03ceb66a7891642e471e64321c4fd6f
sho-gun <masakihosono@gmail.com>
2020-01-19 19:16:13 +0900

    two layer net

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import os
 import numpy as np
-from module.ActivationFunction import softmax
+from module.ActivationFunction import sigmoid, softmax
 from module.LossFunction import cross_entropy_error
 from module.Gradient import numerical_gradient
 
-class simpleNet:
+class SimpleNet:
     def __init__(self):
         self.W = np.random.randn(2,3)
 
@@ -18,8 +18,51 @@ class simpleNet:
 
         return loss
 
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        # init weights
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+    def predict(self, x):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        return y
+
+    def loss(self, x, t):
+        y = self.predict(x)
+        return cross_entropy_error(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
+
 if __name__ == '__main__':
-    net = simpleNet()
+    net = SimpleNet()
     print(net.W)
 
     x = np.array([0.6, 0.9])
